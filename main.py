d53687a86f43d184d9d2e02f43ce99e940a5c3a9
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-19 09:38:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ from gtts import gTTS
 import simpleaudio as sa
 import os
 
-text="""
-Perkenalan nama Saya Rofi.
-Saya dari Bekasi.
-"""
+def text_to_speech(text, lang='id', pitch=0.7, speed=0.8):
+    # Mengatur pitch dan speed
+    tts = gTTS(text=text, lang=lang, slow=False)
+    tts.speed = speed
+    tts.pitch = pitch
+    tts.save("output.mp3")
+    # Konversi file MP3 ke WAV
+    os.system("ffmpeg -loglevel panic -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
+    # Memutar file WAV
+    wave_obj = sa.WaveObject.from_wave_file("output.wav")
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+    # Hapus file setelah selesai diputar
+    os.remove("output.mp3")
+    os.remove("output.wav")
 
-tts = gTTS(text=text, lang="id")
-tts.save("output.mp3")
-# Menggunakan ffmpeg untuk mengonversi file MP3 ke WAV
-os.system("ffmpeg -loglevel panic -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
-# Memuat file WAV menggunakan simpleaudio
-wave_obj = sa.WaveObject.from_wave_file("output.wav")
-play_obj = wave_obj.play()
-play_obj.wait_done()
-# Menghapus file setelah selesai diputar
-os.remove("output.mp3")
-os.remove("output.wav")
+if __name__ == "__main__":
+    text = input("Masukkan teks yang ingin diubah menjadi suara: ")
+    text_to_speech(text, pitch=0.7, speed=0.8)
