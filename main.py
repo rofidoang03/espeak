aa5fa58934d56f579f44ef3e366c013fea8a1eee
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-19 09:32:31 +0700

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ import os
 
 def text_to_speech(text, lang='id'):
     tts = gTTS(text=text, lang=lang)
-    filename = "output.wav"
     tts.save("output.mp3")
-    os.system("ffmpeg -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
-    wave_obj = sa.WaveObject.from_wave_file(filename)
+    # Menggunakan ffmpeg untuk mengonversi file MP3 ke WAV
+    os.system("ffmpeg -loglevel panic -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
+    # Memuat file WAV menggunakan simpleaudio
+    wave_obj = sa.WaveObject.from_wave_file("output.wav")
     play_obj = wave_obj.play()
     play_obj.wait_done()
+    # Menghapus file setelah selesai diputar
     os.remove("output.mp3")
-    os.remove("output.wav")  # Hapus file setelah diputar untuk kebersihan
+    os.remove("output.wav")
 
 if __name__ == "__main__":
     text = input("Masukkan teks yang ingin diubah menjadi suara: ")
