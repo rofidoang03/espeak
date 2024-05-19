474050900e04d6539babc8b36ca382cf18b3813c
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-19 20:33:57 +0700

    Update main.sh

--- main.sh
+++ main.sh
@@ -10,10 +10,14 @@ fi
 
 CAPFILE=$1
 
-# Gunakan tshark untuk memindai file capture dan mencari paket EAPOL
+# Perbaiki file capture yang mungkin rusak menggunakan editcap
+TEMP_CAPFILE=$(mktemp /tmp/fixed_capture.XXXXXX.cap)
+editcap -F libpcap "$CAPFILE" "$TEMP_CAPFILE"
+
+# Gunakan tshark untuk memindai file capture yang telah diperbaiki dan mencari paket EAPOL
 # Filter: eapol
 # Kolom yang diekstrak: bssid, frame.number
-tshark -r "$CAPFILE" -Y "eapol" -T fields -e wlan.bssid -e frame.number | sort | uniq -c | awk '$1 == 4 {print $2}' > bssid_list.txt
+tshark -r "$TEMP_CAPFILE" -Y "eapol" -T fields -e wlan.bssid -e frame.number | sort | uniq -c | awk '$1 == 4 {print $2}' > bssid_list.txt
 
 if [ -s bssid_list.txt ]; then
     echo "BSSID with 4 EAPOL packets found:"
@@ -22,5 +26,5 @@ else
     echo "No BSSID found with 4 EAPOL packets."
 fi
 
-# Opsional: Hapus file sementara
-# rm -f bssid_list.txt
+# Hapus file sementara
+rm -f "$TEMP_CAPFILE" bssid_list.txt
