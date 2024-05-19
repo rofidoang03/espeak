9a7b5f09fe824218c2b1edc0a62cd0434e53b73b
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-19 09:58:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,42 @@ from gtts import gTTS
 import simpleaudio as sa
 import os
 
-def text_to_speech(text, lang='id', pitch=0.6, speed=0.8):
-    # Mengatur pitch dan speed
-    tts = gTTS(text=text, lang=lang, slow=False)
-    tts.speed = speed
-    tts.pitch = pitch
-    tts.save("output.mp3")
-    # Konversi file MP3 ke WAV
-    os.system("ffmpeg -loglevel panic -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
-    # Memutar file WAV
-    wave_obj = sa.WaveObject.from_wave_file("output.wav")
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
-    # Hapus file setelah selesai diputar
-    os.remove("output.mp3")
-    os.remove("output.wav")
+text="""
+PERINGATAN
 
-if __name__ == "__main__":
-    text = input("Masukkan teks yang ingin diubah menjadi suara: ")
-    # Mengatur pitch menjadi lebih rendah (misalnya, 0.5) untuk suara cowok
-    text_to_speech(text, pitch=0.5)
+Penyalahgunaan atau penggunaan program ini untuk
+aktivitas ilegal dilarang keras. Program ini
+disediakan semata-mata untuk tujuan pembelajaran dan
+pengujian keamanan jaringan yang sah. Pengguna
+bertanggung jawab penuh atas penggunaan dan
+konsekuensi yang timbul dari penggunaan program ini.
+
+DILARANG KERAS menggunakan program ini tanpa izin
+atau untuk tujuan merusak atau melanggar hukum.
+Segala tindakan ilegal diluar tanggung jawab
+pembuat program.
+
+DENGAN MENGGUNAKAN PROGRAM INI, ANDA MENYATAKAN
+BAHWA ANDA MEMAHAMI DAN MENERIMA SYARAT DAN KETENTUAN
+DIATAS.
+"""
+
+lang="id"
+
+print("[*] Dengarkan Saya...")
+
+# Membuat suara dari teks
+tts = gTTS(text=text, lang=lang)
+tts.save("output.mp3")
+
+# Menggunakan ffmpeg untuk mengonversi file MP3 ke WAV
+os.system("ffmpeg -loglevel panic -i output.mp3 -acodec pcm_s16le -ac 2 -ar 44100 output.wav")
+
+# Memuat file WAV menggunakan simpleaudio
+wave_obj = sa.WaveObject.from_wave_file("output.wav")
+play_obj = wave_obj.play()
+play_obj.wait_done()
+
+# Menghapus file setelah selesai diputar
+os.remove("output.mp3")
+os.remove("output.wav")
