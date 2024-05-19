34943b63c3e5f0aec39d13194cad8a3dd2ed731a
rofidoang03 <149921715+rofidoang03@users.noreply.github.com>
2024-05-19 09:26:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,12 @@ from gtts import gTTS
 from playsound import playsound
 import os
 
-def text_to_speech(text, lang='id'):
-    tts = gTTS(text=text, lang=lang)
-    filename = "output.mp3"
-    tts.save(filename)
-    playsound(filename)
-    os.remove(filename)  # Hapus file setelah diputar untuk kebersihan
+text="""
+Perkenalkan nama Saya Rofi.
+Saya lahir di Bekasi."""
 
-if __name__ == "__main__":
-    text = input("Masukkan teks yang ingin diubah menjadi suara: ")
-    text_to_speech(text)
+tts=gTTS(text=text, lang="id")
+nama_file="sound.mp3"
+tts.save(nama_file)
+playsound(nama_file)
+os.remove(nama_file)
